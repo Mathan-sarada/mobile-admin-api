d5a1e0a7ea1b4e5e98fe5785f4a0bf50ea8c448a
Mathan-sarada <saradamathan2311@gmail.com>
2020-10-29 23:13:59 +0530

    add status

--- src/db/order_details.js
+++ src/db/order_details.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 let schema = new mongoose.Schema({
     orderDetails: { type: mongoose.Schema.Types.Mixed },
     isVerified:{type:Boolean,required:true},
-    vendor_email:{type:String,required:true}
+    vendor_email:{type:String,required:true},
+    status:{type:String,default:"Unpaid"}
 
 }, {
     timestamps: { createdAt: 'created_date', updatedAt: 'modified_date' }
